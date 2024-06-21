d2278b61d1331bd24010bff7cbf4713714a19b52
chintan <chintan.tops@gmail.com>
2024-06-21 15:03:01 +0530

    all

---
+++ Learning_corejava/src/basic/Datatypes.java
@@ -0,0 +1,46 @@
+package basic;
+
+public class Datatypes {
+	public static void main(String[] args) {
+		
+		
+//		byte b = -128;
+//		short s = 120;
+//		int i = 5666;
+//		long l = 56565;
+//		
+//		
+//		float f = 65.3325656565f;
+//		double d = 565656.56565656565;
+//		
+//		char ch = 'A';
+//		
+//		boolean bool = false;
+		
+//		String str = "Tejas tadvi";
+//		
+//		System.out.println(f);
+//		System.out.println(d);
+		
+		
+//		int x = 'D';
+//		System.out.println(x);
+//		
+//		char y = 65;
+//		System.out.println(y);
+		
+		
+		//datatype casting
+		
+		
+		//widning -implicite
+		int a = 152; //4
+		long b = a;  //8
+		
+		//nerrwoing - explcite
+		long x = 56565; //8
+		int y = (int) x;      //4
+		
+		
+	}
+}
---
+++ Learning_corejava/src/operators/O002_Unary.java
@@ -0,0 +1,21 @@
+package operators;
+
+public class O002_Unary {
+	 public static void main(String[] args) {
+		
+		 
+		 //++pre, --pre, post++,post--
+		 
+		 
+		 int a = 10;
+		 int b = a-- + a-- - ++a + a++ - a++;
+		       
+		         
+		        
+		 System.out.println("a : "+a); //11 10
+		 System.out.println("b : "+b); //47 7
+
+		 
+		 
+	}
+}
